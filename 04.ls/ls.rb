7a3c0338f53eea64a81aba58955c18c3cb061e16
s-kuroki <s-kuroki@beingcorp.co.jp>
2026-04-08 08:05:58 +0000

    1回目提出後の指摘事項対応

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,25 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COLUMNS = 3
+
 def main
   files = fetch_files
   display_files(files)
 end
 
 def fetch_files
-  Dir.glob('*').sort
+  Dir.glob('*')
 end
 
 def display_files(files)
-  columns = 3
-  rows = (files.size.to_f / columns).ceil
+  rows = files.size.ceildiv(COLUMNS)
+  width = files.map { |f| f.encode("EUC-JP").bytesize }.max + 2
   nested_files = files.each_slice(rows).to_a
-  nested_files.map! { |column| column.fill(nil, column.size...rows) }
+  nested_files.map { |column| column.fill(nil, column.size...rows) }
   nested_files.transpose.each do |row|
     row.each do |file|
-      print file.ljust(16) if file
+      next if file.nil?
+      padding_size = width - file.encode("EUC-JP").bytesize
+      print file + (" " * padding_size) if file
     end
-    puts ''
+    puts
   end
 end
 
